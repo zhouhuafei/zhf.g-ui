9de213d9f00b395d1319bf6dcec8d967576fdc24
zhouhuafei <zhouhuafei@shopex.cn>
2018-09-03 12:05:45 +0800

    md

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,20 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // html生成的插件
 
+// 环境----开发环境
+let configEnvironment = {
+    hash: '[hash:8].', // 图片和字体用到了这个hash
+    chunkhash: '', // js用到了这个chunkhash
+    contenthash: '', // css用到了这个contenthash
+    min: '', // 第三方库是否引用压缩版(生产环境引用压缩版)
+    isMinCss: false, // 是否压缩css
+    isWatch: true, // 是否监听
+    minView: {}, // 压缩视图模板文件
+    devtool: '', // #source-map
+};
+
 module.exports = {
+    // devtool----#source-map
+    devtool: configEnvironment.devtool,
     entry: {
         'index': './src/demo/index.js', // 入口文件
     },
@@ -18,6 +32,83 @@ module.exports = {
     ],
     watch: true,
     module: {
-        rules: [],
+        // rules----loader加载器的规则集合
+        rules: [
+            // loader----处理sass
+            {
+                test: /\.(css|scss)$/,
+                exclude: /(node_modules|bower_components)/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            minimize: configEnvironment.isMinCss, // css压缩
+                        },
+                    }, 'postcss-loader', 'sass-loader'],
+                }),
+            },
+            // loader----es6转成es5
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: ['babel-loader'],
+            },
+            // loader----处理图片
+            {
+                test: /\.(png|jp(e)?g|gif|svg|ico)(\?.*)?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: `images/[name].${configEnvironment.hash}[ext]`,
+                        },
+                    },
+                ],
+            },
+            // loader----处理字体
+            {
+                test: /\.(woff|eot|ttf)(\?.*)?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: `fonts/[name].${configEnvironment.hash}[ext]`,
+                        },
+                    },
+                ],
+            },
+            // loader----处理音频
+            {
+                test: /\.(mp3)(\?.*)?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: `audios/[name].${configEnvironment.hash}[ext]`,
+                        },
+                    },
+                ],
+            },
+            // loader----处理视图模板文件里的src
+            {
+                test: /\.ejs/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            attrs: ['img:src', 'img:data-src', 'link:href', 'audio:src'],
+                        },
+                    },
+                ],
+            },
+        ],
     },
 };
