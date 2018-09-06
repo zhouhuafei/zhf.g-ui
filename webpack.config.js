b884332cf8106b055fe3c6660e0477a494161d8c
zhouhuafei <zhouhuafei@shopex.cn>
2018-09-06 13:39:16 +0800

    css使用mini-css-extract-plugin模块进行提取。

--- webpack.config.js
+++ webpack.config.js
@@ -99,14 +99,15 @@ module.exports = function (env, argv) {
         output: {
             path: `${__dirname}/dist/demo/`, // 出口路径，必须是绝对路径
             filename: `js/pages/[name].${configEnvironment.chunkhash}js`, // 出的文件名
-            publicPath: `/`, // 引入静态资源文件时，资源的前缀路径。
+            publicPath: `/`, // 引入静态资源文件时，资源的前缀路径。如果不使用webpack-dev-server，路径配置成'../../'，静态页面可以直接本地打开进行观看。
             /*
             chunkFilename用来打包require.ensure方法中引入的模块,如果该方法中没有引入任何模块则不会生成任何chunk块文件
             比如在main.js文件中,require.ensure([],function(require){alert(11);}),这样不会打包块文件
             只有这样才会打包生成块文件require.ensure([],function(require){alert(11);require('./greeter')})
             或者这样require.ensure(['./greeter'],function(require){alert(11);})
             chunk的hash值只有在require.ensure中引入的模块发生变化,hash值才会改变
-            注意:对于不是在ensure方法中引入的模块,此属性不会生效,只能用CommonsChunkPlugin插件来提取
+            注意1：对于不是在ensure方法中引入的模块,此属性不会生效,只能用CommonsChunkPlugin插件来提取。
+            注意2：CommonsChunkPlugin在webpack4中不支持了，支持使用optimization.splitChunks属性进行公共模块(chunks)的提取。css使用mini-css-extract-plugin模块进行提取。
             */
             chunkFilename: `js/chunks/[name].${configEnvironment.chunkhash}js`,
         },
