6086fb6b123c8dbb155bbd129296598ccb1031f3
zhouhuafei <zhouhuafei@shopex.cn>
2018-09-10 17:39:16 +0800

    优化g-progress-bar以及css边框测试待续。。。

--- src/ui/js/components/g-progress-bar.js
+++ src/ui/js/components/g-progress-bar.js
@@ -37,21 +37,22 @@ class Sub extends Super {
         self.canvasContext = self.canvas.getContext('2d');
         self.radian = Math.PI / 180; // 弧度
         self.canvasSetVar(); // 设置变量
-        self.canvasDrawMap(); // 画地图
+        self.canvasDrawCircle(); // 画圆
         self.canvasSetScale(0); // 设置比例
         window.addEventListener('resize', function () {
             self.canvasSetVar(); // 设置变量
-            self.canvasDrawMap(); // 画地图
+            self.canvasDrawCircle(); // 画圆
             self.canvasSetScale(self.canvasScaleNum || 0); // 设置比例
         });
     }
 
-    canvasSetVar() {
+    canvasSetVar() { // 设置变量
         const moduleDom = this.moduleDom;
         const canvas = this.canvas;
         const canvasContext = this.canvasContext;
         this.canvasW = moduleDom.offsetWidth;
         this.canvasH = moduleDom.offsetHeight;
+        console.log(getComputedStyle(moduleDom).borderWidth);
         canvas.width = this.canvasW; // 宽
         canvas.height = this.canvasH; // 高
         this.canvasRadius = this.canvasW / 2; // 半径
@@ -59,14 +60,14 @@ class Sub extends Super {
         canvasContext.rotate(-90 * this.radian); // 旋转90弧度
         this.canvasCenterX = 0; // 中心点x
         this.canvasCenterY = 0; // 中心点y
-        this.canvasBorder = 5; // 边框
+        this.canvasBorderW = 5; // 边框
     }
 
-    canvasDrawMap(strokeStyle, angle) {
+    canvasDrawCircle(strokeStyle, angle) { // 画圆
         const ctx = this.canvasContext;
         ctx.strokeStyle = strokeStyle || '#dddddd';
         let temporaryRadius = this.canvasRadius; // 临时半径
-        for (let i = 0; i < this.canvasBorder; i++) {
+        for (let i = 0; i < this.canvasBorderW; i++) {
             ctx.beginPath();
             ctx.arc(this.canvasCenterX, this.canvasCenterY, temporaryRadius, 0, `${!isNaN(angle) ? angle * this.radian : 360 * this.radian}`, false);
             ctx.stroke();
@@ -78,8 +79,8 @@ class Sub extends Super {
     canvasSetScale(num = 0) {
         this.canvasScaleNum = num;
         this.canvasContext.clearRect(0, 0, this.canvasW, this.canvasH); // 清理画布
-        this.canvasDrawMap(); // 画地图
-        this.canvasDrawMap('#67C23A', num * 3.6); // 画地图
+        this.canvasDrawCircle(); // 画圆
+        this.canvasDrawCircle('#67C23A', num * 3.6); // 画圆
         this.moduleDom.querySelector('.g-progress-bar-text').innerHTML = `${num}%`;
     }
 }
---
+++ src/ui/js/config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    psdWidth: 320, // 设计图的宽。(自适应的时候，这里会被用到)。
+    containerMaxWidth: 'body', // 最外层的容器(仅支持选择器格式，例如：'.wrap'，'#container')。
+};
