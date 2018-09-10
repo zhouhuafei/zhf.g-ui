dfda6dd1e47b7679024f12ceecfca1e84cbe52e8
zhouhuafei <zhouhuafei@shopex.cn>
2018-09-10 19:48:59 +0800

    canvas自适应

--- src/ui/js/components/g-progress-bar.js
+++ src/ui/js/components/g-progress-bar.js
@@ -1,5 +1,6 @@
 const extend = require('zhf.extend'); // 对象的扩展
 const createElement = require('zhf.create-element'); // 创建元素
+const config = require('../config/config'); // 超类型(子类型继承的对象)
 const Super = require('../components-super/g-super-es6'); // 超类型(子类型继承的对象)
 
 // 子类型
@@ -39,6 +40,7 @@ class Sub extends Super {
         self.canvasSetVar(); // 设置变量
         self.canvasSetScale(0); // 设置比例
         window.addEventListener('resize', function () {
+            config.outermostContainerMaxWidth = document.documentElement.clientWidth; // 重置最大宽度
             self.canvasSetVar(); // 设置变量
             self.canvasSetScale(self.canvasScaleNum || 0); // 设置比例
         });
@@ -57,7 +59,7 @@ class Sub extends Super {
         canvasContext.rotate(-90 * this.radian); // 旋转90弧度
         this.canvasCenterX = 0; // 中心点x
         this.canvasCenterY = 0; // 中心点y
-        this.canvasBorderW = 5; // 边框
+        this.canvasBorderW = config.px2remCanvas(5); // 边框
     }
 
     canvasDrawCircle(strokeStyle, angle) { // 画圆
--- src/ui/js/config/config.js
+++ src/ui/js/config/config.js
@@ -1,4 +1,25 @@
-module.exports = {
-    psdWidth: 320, // 设计图的宽。(自适应的时候，这里会被用到)。
-    containerMaxWidth: 'body', // 最外层的容器(仅支持选择器格式，例如：'.wrap'，'#container')。
-};
+class Config {
+    constructor() {
+        this.outermostContainer = window['g-outermost-container'] || 'body'; // 这里需要手动配置。最外层的容器(仅支持选择器格式，例如：'.wrap'，'#container')。
+        this.outermostContainerMaxWidth = document.querySelector(this.outermostContainer).offsetWidth; // 最外层容器的最大宽度。,
+        this.isH5 = window['g-is-h5'] ? window['g-is-h5'] : true; // 这里需要手动配置。是否是手机自适应页面(canvas自适应用到了这个，手机端才有canvas自适应)。
+        this.h5PsdWidth = window['g-h5-psd-width'] || 320; // 这里需要手动配置。设计图的宽。(如果是手机自适应站点，这里才会被使用到)。
+        this.h5PsdSplitNum = window['g-h5-psd-split-num'] || 10; // 这里需要手动配置。页面分割数量。在此处表达的意思是：将最外层容器的宽度分成10份，则一份就是outermostContainerMaxWidth/10像素，把结果设置给html的fontSize属性，则1rem就是outermostContainerMaxWidth/10像素。
+        this.px2remCanvas = function (px) { // canvas自适应
+            if (this.isH5) { // 如果是h5，canvas才有自适应功能
+                return px * this.outermostContainerMaxWidth / this.h5PsdWidth;
+            } else {
+                return px;
+            }
+        };
+        this.px2rem = function (px) { // rem自适应
+            if (this.isH5) { // 如果是h5，才有自适应功能
+                return px / this.h5PsdWidth * this.h5PsdSplitNum;
+            } else {
+                return px;
+            }
+        };
+    }
+}
+
+module.exports = new Config();
