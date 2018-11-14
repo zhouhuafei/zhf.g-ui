29a311f076ef8c821ea5af83b77e7c5db38e46f7
zhouhuafei <1123486116@qq.com>
2018-11-14 11:50:38 +0800

    md

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -1,45 +1,71 @@
 const path = require('path');
 const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin'); // 压缩js
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin'); // 将单个文件或整个目录复制到构建目录
+const CleanWebpackPlugin = require('clean-webpack-plugin'); // 清空目录
 
-module.exports = {
-    entry: {
-        'index': './src/js/components_vue/g-sort-tab/index.js',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        library: 'VueDraggableResizable',
-        libraryTarget: 'umd',
-    },
-    devtool: false,
-    resolve: {
-        extensions: ['.js', '.vue'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                exclude: /node_modules/,
-            },
+module.exports = function (env, argv) {
+    const isProduction = argv.mode === 'production'; // 是否是生产环境
+    console.log('argv.mode', argv.mode);
+    // 压缩----配置
+    const minimizer = [];
+    // 环境----生产环境
+    if (isProduction) {
+        minimizer.push(new UglifyJsPlugin({cache: true, parallel: true, sourceMap: false})); // 插件----压缩js
+    }
+    return {
+        entry: {
+            'index': './src/js/components_dom/g-confirm/index.js',
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'js/[name].js',
+            library: 'VueDraggableResizable',
+            libraryTarget: 'umd',
+        },
+        devtool: false,
+        resolve: {
+            extensions: ['.js', '.vue'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        externals: {
+            vue: 'vue',
+        },
+        // 优化----配置
+        optimization: {
+            minimizer: minimizer,
+        },
+        plugins: [
+            // 插件----清空dist目录
+            new CleanWebpackPlugin(['dist'], {
+                root: __dirname,
+                verbose: true,
+                dry: false,
+            }),
+            // 插件----vue-loader
+            new VueLoaderPlugin(),
+            // 插件----js转模块
+            new webpack.DefinePlugin({
+                'process.env': {
+                    NODE_ENV: '"production"',
+                },
+            }),
+            //  插件----将单个文件或整个目录复制到构建目录
+            new CopyWebpackPlugin([{from: 'src/scss', to: 'scss'}]),
         ],
-    },
-    externals: {
-        vue: 'vue',
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"',
-            },
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: false,
-        }),
-    ],
+    };
 };
